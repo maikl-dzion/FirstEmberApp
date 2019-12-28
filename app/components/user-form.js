5aa28de2f476c7ee49575222d788b30faf9d1de0
Abasov Maikl <abasov.m@basis-plus.ru>
2019-12-28 18:18:52 +0300

    first commit

---
+++ app/components/user-form.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import Component from '@glimmer/component';
+
+export default class UserFormComponent extends Component {
+    // didRender() {
+    //     Materialize.updateTextFields();
+    // }
+}
